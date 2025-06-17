44e26b8a8ab1abee5ac32678463e1bca176c1887
GBathie <gbathie.dev@gmail.com>
2025-06-17 09:44:46 +0200

    Generate examples

--- sat_solver.py
+++ sat_solver.py
@@ -1,9 +1,9 @@
-import copy
+import random
+import time
 from typing import Tuple
-import cvc5
 
-# import utils
-from cvc5 import TermManager, Kind
+import cvc5
+from cvc5 import Kind, TermManager
 
 type Input = list[int]
 type Output = int
@@ -100,14 +100,87 @@ def print_synth_solutions(terms, sols):
     print(result)
 
 
+class FormulaNode:
+    def __init__(self, name, op, *children) -> None:
+        self.name = name
+        self.op = op
+        self.children = children
+
+    def eval(self, inputs):
+        eval_children = [c.eval(inputs) for c in self.children]
+        return self.op(inputs, *eval_children)
+
+    def to_string(self, prefix="", last=True) -> str:
+        r = prefix + ("└── " if last else "├── ") + self.name + "\n"
+        for i, c in enumerate(self.children):
+            next_last = i == len(self.children) - 1
+            r += c.to_string(prefix + ("    " if last else "|   "), next_last)
+        # str_children = [c.to_string(prefix + "|   ") for c in self.children]
+        # r = prefix + "├── " + self.name + "\n" + "".join(str_children)
+        return r
+
+
+# vars = {i: (lambda inputs, *children: print(len(inputs), i) or inputs[i]) for i in range(10)}
+unary = {"not": lambda inputs, *children: ~children[0]}
+binary = {
+    "or": lambda inputs, *children: children[0] | children[1],
+    "and": lambda inputs, *children: children[0] & children[1],
+}
+
+
+def random_formula(size: int, n_vars: int):
+    if size == 1:
+        i = random.randrange(n_vars)
+
+        def v(inputs, *children):
+            return inputs[i]
+
+        n = FormulaNode(f"x_{i}", v)
+        return n
+    elif size == 2:
+        s = random_formula(1, n_vars)
+        n = FormulaNode("not", unary["not"], s)
+        return n
+    else:
+        left = random.randint(1, size - 2)
+        right = size - 1 - left
+        f_left = random_formula(left, n_vars)
+        f_right = random_formula(right, n_vars)
+        op = random.choice(list(binary.keys()))
+        f = binary[op]
+        return FormulaNode(op, f, f_left, f_right)
+
+
+def gen_examples(f: FormulaNode, n_bits: int, n_vars: int, n_examples: int) -> Task:
+    ub = 1 << n_bits - 1
+    task = []
+    for _ in range(n_examples):
+        inputs = [random.randint(0, ub) for _ in range(n_vars)]
+        output = f.eval(inputs)
+        task.append((inputs, output))
+    return task
+
+
 if __name__ == "__main__":
-    solve_cvc5(
-        [
-            ([0, 1], 0),
-            ([1, 1], 1),
-            ([1, 0], 0),
-            ([0, 0], 0),
-        ],
-        bit_vec_size=16,
-        n_vars=2,
-    )
+    # solve_cvc5(
+    #     [
+    #         ([0, 1], 0),
+    #         ([1, 1], 1),
+    #         ([1, 0], 0),
+    #         ([0, 0], 0),
+    #     ],
+    #     bit_vec_size=16,
+    #     n_vars=2,
+    # )
+
+    N_BITS = 16
+    N_VARS = 8
+    N_EXAMPLES = 200
+    F_SIZE = 15
+    f = random_formula(F_SIZE, N_VARS)
+    print(f.to_string())
+    ex = gen_examples(f, N_BITS, N_VARS, N_EXAMPLES)
+    start = time.time()
+    solve_cvc5(ex, N_BITS, N_VARS)
+    elapsed = time.time() - start
+    print(f"Time: {elapsed:.3f}s")
