40f3b983862abfcfc73155852507cfee44f473be
Gohu2000 <hugo.barzu@gmail.com>
2025-07-15 19:55:37 +0200

    correction bug

--- bitvector-diff/start_run.sh
+++ bitvector-diff/start_run.sh
@@ -1,2 +1,4 @@
-$(oarsub -l host=1,walltime=1:00:00 -p "cluster='paradoxe'" /home/gbathie/synthese_MBA/bitvector-diff/run.sh -q default)
+eval $(oarsub -l host=1,walltime=1:00:00 -p "cluster='paradoxe'" /home/gbathie/synthese_MBA/bitvector-diff/run.sh -q default)
+echo OAR.${OAR_JOB_ID}.stdout
+echo OAR.${OAR_JOB_ID}.stderr
 watch wc -l OAR.${OAR_JOB_ID}.stdout
