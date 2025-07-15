5eb0328d175073a2c89ae44032db2c465022ce68
Gohu2000 <hugo.barzu@gmail.com>
2025-07-15 19:44:59 +0200

    modifications de start_run

--- bitvector-diff/start_run.sh
+++ bitvector-diff/start_run.sh
@@ -1,1 +1,2 @@
-oarsub -l host=1,walltime=1:00:00 -p "cluster='paradoxe'" /home/gbathie/synthese_MBA/bitvector-diff/run.sh -q default | $(python3 /home/gbathie/synthese_MBA/bitvector-diff/watch_stdout.py)
+$(oarsub -l host=1,walltime=1:00:00 -p "cluster='paradoxe'" /home/gbathie/synthese_MBA/bitvector-diff/run.sh -q default)
+watch wc -l OAR.${OAR_JOB_ID}.stdout
