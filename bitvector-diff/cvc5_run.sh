20f62f7f660838756746919bb02a171dd3531acd
Gohu2000 <hugo.barzu@gmail.com>
2025-07-17 11:11:03 +0200

    correction bug cvc5

--- bitvector-diff/cvc5_run.sh
+++ bitvector-diff/cvc5_run.sh
@@ -3,4 +3,5 @@
 cd /home/gbathie/synthese_MBA/bitvector-diff
 cargo build -r
 
+source cvc5_env/bin/activate
 python3 cvc5_command_lines.py | parallel -j25 --joblog ./parallel.log --ssh oarsh --slf $OAR_NODEFILE
