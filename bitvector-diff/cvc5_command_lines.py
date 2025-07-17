842eb68f41ffba46c9465ee4c21f45f5be5b852d
Gohu2000 <hugo.barzu@gmail.com>
2025-07-17 11:29:48 +0200

    encore une tentative...

--- bitvector-diff/cvc5_command_lines.py
+++ bitvector-diff/cvc5_command_lines.py
@@ -4,4 +4,4 @@ folder = "/home/gbathie/synthese_MBA/bitvector-diff/instances"
 timeout = 60
 
 for i in glob(folder + "/*.json"):
-    print(f"timeout {timeout} python3 /home/gbathie/synthese_MBA/sat_solver.py {i}")
+    print(f"source /home/gbathie/synthese_MBA/cvc5_env/bin/activate && timeout {timeout} python3 /home/gbathie/synthese_MBA/sat_solver.py {i}")
