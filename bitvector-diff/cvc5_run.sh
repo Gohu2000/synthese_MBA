736a350c05f0597b44c77d947a10db10ff3e361e
GBathie <gbathie.dev@gmail.com>
2025-07-22 10:34:06 +0200

    Update scripts

--- bitvector-diff/cvc5_run.sh
+++ bitvector-diff/cvc5_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /home/gbathie/synthese_MBA/bitvector-diff
-cargo build -r
+cd ~/synthese_MBA/
+source .venv/bin/activate
 
-source cvc5_env/bin/activate
+
 python3 cvc5_command_lines.py | parallel -j25 --joblog ./parallel.log --ssh oarsh --slf $OAR_NODEFILE
