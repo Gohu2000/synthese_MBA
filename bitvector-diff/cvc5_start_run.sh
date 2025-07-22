978b71b7a0c8767bb40cb7a73449d1abebf8b8eb
Gabriel Bathie <gbathie@frennes.rennes.grid5000.fr>
2025-07-22 10:43:08 +0200

    move cvc5 files

--- bitvector-diff/cvc5_start_run.sh
+++
@@ -1,5 +0,0 @@
-eval $(oarsub -l host=1,walltime=1:00:00 -p "cluster='paradoxe'" /home/gbathie/synthese_MBA/bitvector-diff/cvc5_run.sh -q default)
-echo OAR.${OAR_JOB_ID}.stdout
-echo OAR.${OAR_JOB_ID}.stderr
-echo scp -r rennes.g5k:~/synthese_MBA/bitvector-diff/OAR.${OAR_JOB_ID}.stdout ./stdout
-watch wc -l OAR.${OAR_JOB_ID}.stdout
