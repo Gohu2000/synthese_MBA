c0117185492fb9e424936cb97912306488adb188
Gohu2000 <hugo.barzu@gmail.com>
2025-08-19 14:56:09 +0200

    init

--- bitvector-diff/init.sh
+++ bitvector-diff/init.sh
@@ -1,4 +1,1 @@
-cd ~/synthese_MBA/bitvector-diff
-cargo build -r
-
-oarsub -l host=1,walltime=1:00:00 -p "cluster='paradoxe'" ~/synthese_MBA/bitvector-diff/target/release/generate_instances -q default
+oarsub -l host=1,walltime=1:00:00 -p "cluster='paradoxe'" ~/synthese_MBA/bitvector-diff/init.sh -q default
