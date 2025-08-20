d1974b1033cb95fabc22c75b3ef7231b3209642c
Gohu2000 <hugo.barzu@gmail.com>
2025-08-20 16:40:19 +0200

    xyntia update

--- xyntia_run.sh
+++ xyntia_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-
+echo autre_test
 cd ~/synthese_MBA/xyntia
-{
+#{
 sudo-g5k apt update
 sudo-g5k apt install -y libgmp3-dev gcc-multilib gdb python3 python3-pip python3-venv openjdk-17-jdk libgmp-dev pkg-config opam
 
@@ -11,7 +11,7 @@ opam init -y --disable-sandboxing
 eval $(opam env)
 opam switch create . 4.14.1 -y
 eval $(opam env)
-} > /dev/null 2>&1
+#} > /dev/null 2>&1
 
 echo test
 python3 xyntia_command_lines.py | parallel -j25 --joblog ./parallel.log --ssh oarsh --slf $OAR_NODEFILE
