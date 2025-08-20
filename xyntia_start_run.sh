0f350e9adf46f5abe0228b3338b20fa67d5e050b
Gohu2000 <hugo.barzu@gmail.com>
2025-08-20 16:13:22 +0200

    xyntia folder

--- xyntia_start_run.sh
+++ xyntia_start_run.sh
@@ -2,4 +2,4 @@ eval $(oarsub -l host=1,walltime=1:00:00 -p "cluster='paradoxe'" ~/synthese_MBA/
 echo OAR.${OAR_JOB_ID}.stdout
 echo OAR.${OAR_JOB_ID}.stderr
 echo scp -r rennes.g5k:~/synthese_MBA/OAR.${OAR_JOB_ID}.stdout ./stdout
-watch wc -l ~/synthese_MBA/xyntia/OAR.${OAR_JOB_ID}.stdout
+watch wc -l ~/synthese_MBA/OAR.${OAR_JOB_ID}.stdout
